0de990def93d06446add674792feb0dcee3b0eca
Teslim <yusuftessy2005@gmail.com>
2025-05-05 12:11:56 +0100

    refix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -74,7 +74,7 @@ export default function Navbar() {
                 src="/logo.png"
                 alt="Ajo on Sol Logo"
                 fill
-                className=" object-cover"
+                className="object-contain"
                 sizes="48px"
                 priority
               />
