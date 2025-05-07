08541e94d8f3ca89e7505d9606a3a55314b087c1
Teslim <yusuftessy2005@gmail.com>
2025-05-07 03:35:42 +0100

    fix: correct import case for PoolDetailsContent component and update webpack alias configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require("path")
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname)
+    return config
+  },
 }
 
-export default nextConfig
+module.exports = nextConfig
